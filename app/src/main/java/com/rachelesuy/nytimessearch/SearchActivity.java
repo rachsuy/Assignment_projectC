7def5dbddb952a12eb02cb4d6d6e31d3ff595c98
Rachele Suy <rachelesuy@gmail.com>
2018-07-15 21:28:11 -0400

    premiere affichage

--- app/src/main/java/com/rachelesuy/nytimessearch/SearchActivity.java
+++ app/src/main/java/com/rachelesuy/nytimessearch/SearchActivity.java
@@ -2,12 +2,33 @@ package com.rachelesuy.nytimessearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
+EditText etQuery;
+GridView gvResults;
+Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.content_search);
+        setupViews();
+    }
+    public void setupViews(){
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+    }
+
+    public void onArticleSearch(View view){
+        String query = etQuery.getText().toString();
+        //Toast.makeText(this,"searching for" +query, Toast.LENGTH_LONG).show();
+       // As
     }
 }
+
