5ac2cde6e3360e05c0fd9063470ae505d0c6866b
Rachele Suy <rachelesuy@gmail.com>
2018-07-16 05:27:38 -0400

    add JSONOBJECT

--- app/src/main/java/com/rachelesuy/nytimessearch/SearchActivity.java
+++ app/src/main/java/com/rachelesuy/nytimessearch/SearchActivity.java
@@ -2,11 +2,20 @@ package com.rachelesuy.nytimessearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
 EditText etQuery;
@@ -28,7 +37,17 @@ Button btnSearch;
     public void onArticleSearch(View view){
         String query = etQuery.getText().toString();
         //Toast.makeText(this,"searching for" +query, Toast.LENGTH_LONG).show();
-       // As
+        AsyncHttpClient client= new AsyncHttpClient();
+        String url ="http://api.nytimes.com/svc/search/v2/articlesearch.json";
+        RequestParams params = new RequestParams();
+        params.put("api-key", "7c282434b282493380199464b5f3ebbb");
+
+        client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+               Log.d("DEBUG", response.toString());
+            }
+        });
     }
 }
 
